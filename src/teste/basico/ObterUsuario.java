7802e5c5842c86080f9209c1c957fe428873f5d1
marciomarquesdesouza <marciomarques.contato@hotmail.com>
2021-05-12 15:37:41 -0300

    Class get user

--- src/teste/basico/ObterUsuario.java
+++ src/teste/basico/ObterUsuario.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 import modelo.basico.Usuario;
 
-public class NovoUsuario {
+public class ObterUsuario {
 
 	public static void main(String[] args) {
 		
@@ -14,13 +14,8 @@ public class NovoUsuario {
 				.createEntityManagerFactory("jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Usuario novoUsuario = new Usuario("Marcio", "marcio@lanche.com.br");
-		
-		em.getTransaction().begin();
-		em.persist(novoUsuario);
-		em.getTransaction().commit();
-		
-		System.out.println("O Id gerado foi: " + novoUsuario.getId());
+		Usuario usuario = em.find(Usuario.class, 7L);
+		System.out.println(usuario.getNome());
 		
 		em.close();
 		emf.close();
